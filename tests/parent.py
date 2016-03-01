68ef8eda3d201b06783f5219ae5868e84e673b28
Lily Lemmer <lily.lemmer@ave81.com>
2016-03-01 01:26:57 -0600

    Add test structure, add first test

---
+++ tests/parent.py
@@ -0,0 +1,8 @@
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../')
+
+try:
+    os.chdir('demo')
+except OSError:
+    pass
---
+++ tests/test_layers.py
@@ -0,0 +1,23 @@
+import os
+
+import pygame
+
+import parent
+import sappho
+
+
+def test_layers():
+    target_surface = pygame.surface.Surface([400, 300])
+    surface_layers = sappho.SurfaceLayers(target_surface, 4)
+
+    # Should use magic method that counts _surface_layers instead!
+    assert len(surface_layers._surface_layers) == 4
+
+    # should have __iter__ instead
+    for surface in surface_layers._surface_layers:
+        assert surface.get_size() == target_surface.get_size()
+
+    surface_layers[0]
+    surface_layers[1]
+    surface_layers[2]
+    surface_layers[3]
