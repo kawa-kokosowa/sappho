5a82fcb91b7a356446f647a8756b0b626666b504
Alice Jenkinson <ed.ch1zuru@gmail.com>
2016-03-09 08:33:57 +0000

    Update SurfaceLayers render test to test overlapping surfaces

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -36,34 +36,36 @@ class TestSurfaceLayers(object):
 
     def test_render(self):
 
-        subsurface_size = (self.TARGET_SURFACE_SIZE[0] / 2,
-                           self.TARGET_SURFACE_SIZE[1] / 2)
+        subsurface_size = (150, 150)
 
         # Create our test surfaces
         background = pygame.surface.Surface(self.TARGET_SURFACE_SIZE)
-        top_left = pygame.surface.Surface(subsurface_size)
-        top_right = pygame.surface.Surface(subsurface_size)
-        bottom_left = pygame.surface.Surface(subsurface_size)
+        rect1 = pygame.surface.Surface(subsurface_size)
+        rect1pos = (100, 100)
+        rect2 = pygame.surface.Surface(subsurface_size)
+        rect2pos = (200, 200)
+        rect3 = pygame.surface.Surface(subsurface_size)
+        rect3pos = (300, 300)
 
         # Fill the surfaces
         background.fill((255, 255, 255))
-        top_left.fill((255, 0, 0))
-        top_right.fill((0, 255, 0))
-        bottom_left.fill((0, 0, 255))
+        rect1.fill((255, 0, 0))
+        rect2.fill((0, 255, 0))
+        rect3.fill((0, 0, 255))
 
         # Create a surface to compare with and blit our test surfaces
         test_surface = pygame.surface.Surface(self.TARGET_SURFACE_SIZE)
         test_surface.blit(background, (0, 0))
-        test_surface.blit(top_left, (0, 0))
-        test_surface.blit(top_right, (subsurface_size[0], 0))
-        test_surface.blit(bottom_left, (0, subsurface_size[1]))
+        test_surface.blit(rect1, rect1pos)
+        test_surface.blit(rect2, rect2pos)
+        test_surface.blit(rect3, rect3pos)
 
         # Create the SurfaceLayers object and fill it with our layers
         surface_layers = sappho.SurfaceLayers(self.target_surface, 4)
         surface_layers[0].blit(background, (0, 0))
-        surface_layers[1].blit(top_left, (0, 0))
-        surface_layers[2].blit(top_right, (subsurface_size[0], 0))
-        surface_layers[3].blit(bottom_left, (0, subsurface_size[1]))
+        surface_layers[1].blit(rect1, rect1pos)
+        surface_layers[2].blit(rect2, rect2pos)
+        surface_layers[3].blit(rect3, rect3pos)
 
         # Render to the target surface
         surface_layers.render()
@@ -72,5 +74,6 @@ class TestSurfaceLayers(object):
         target_view = self.target_surface.get_view().raw
         test_view = test_surface.get_view().raw
 
-        for i, pixel in enumerate(target_view):
-            assert(pixel == test_view[i])
+        # The returned value is a bytes (str in python2) object so we can 
+        # just do a straight compare
+        assert(target_view == test_view)
