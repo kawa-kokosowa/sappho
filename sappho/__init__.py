024b30b055486df305549fb9e22b7aa5596def27
Alice Jenkinson <ed.ch1zuru@gmail.com>
2016-03-10 02:18:18 +0000

    Version bump to 0.3.2

--- sappho/__init__.py
+++ sappho/__init__.py
@@ -1,5 +1,5 @@
 if __name__ == "__main__":
-    __version__ = "0.3.1"
+    __version__ = "0.3.2"
 else:
     from animatedsprite import AnimatedSprite
     from tilemap import TileMap, Tilesheet, tmx_file_to_tilemaps
