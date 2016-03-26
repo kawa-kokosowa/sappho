e45db1ac487939772f7a354054113faac3d82ec0
Lily Lemmer <lily.lemmer@ave81.com>
2016-03-26 14:11:24 -0500

    Pin to 0.4.3

--- sappho/__init__.py
+++ sappho/__init__.py
@@ -1,5 +1,5 @@
 if __name__ == "__main__":
-    __version__ = "0.4.2"
+    __version__ = "0.4.3"
 else:
     from animatedsprite import AnimatedSprite
     from tilemap import TileMap, Tilesheet, tmx_file_to_tilemaps
