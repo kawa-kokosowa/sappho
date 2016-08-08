9794b036529ddba42c983eb774454b3f22038edb
Lily Seabreeze <lillian.gardenia.seabreeze@gmail.com>
2016-08-08 18:28:38 -0500

    fix for python 3

--- sappho/__init__.py
+++ sappho/__init__.py
@@ -1,8 +1,8 @@
 if __name__ == "__main__":
     __version__ = "0.13.0"
 else:
-    import animatedsprite
-    import collisionsprite
-    import tilemap
-    import layers
-    import camera
+    from . import animatedsprite
+    from . import collisionsprite
+    from . import tilemap
+    from . import layers
+    from . import camera
